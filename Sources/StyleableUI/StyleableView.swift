ca20304e33da7aaeccc742cfb9cf2d026de41c03
ddanilyuk <danis.danilyuk@gmail.com>
2021-05-13 18:18:52 +0300

    Separate to extensions, add StyleGet, add Predifined styles, add StyleableView + Array

--- Sources/StyleableUI/StyleableView.swift
+++ Sources/StyleableUI/StyleableView.swift
@@ -11,36 +11,95 @@ public protocol StyleableView { }
 
 extension UIView: StyleableView { }
 
-public typealias Style<T> = (T) -> Void
+// MARK: - Typealiases
 
+public typealias Style<T> = (T) -> Void
+public typealias StyleGet<T, V> = (T) -> V
+
+// MARK: - Apply
+
+// Applying style without return
 public extension StyleableView {
     
-    // Applying style
     func apply(style: Style<Self>) {
         style(self)
     }
     
     func apply(styles: Style<Self>...) {
-        styles.forEach(apply(style:))
+        styles.forEach { apply(style: $0) }
     }
     
     func apply(styles: [Style<Self>]) {
-        styles.forEach(apply(style:))
+        styles.forEach { apply(style: $0) }
     }
+}
+
+// MARK: - Style
+
+// Applying style and return self
+public extension StyleableView {
     
-    // Applying style and returning self
-    func with(_ style: Style<Self>) -> Self {
+    func style(_ style: Style<Self>) -> Self {
         apply(style: style)
         return self
     }
     
-    func styled(using styles: Style<Self>...) -> Self {
+    func style(using styles: Style<Self>...) -> Self {
         apply(styles: styles)
         return self
     }
     
-    func styled(using styles: [Style<Self>]) -> Self {
+    func style(using styles: [Style<Self>]) -> Self {
         apply(styles: styles)
         return self
     }
+}
+
+// MARK: - StyleGet
+
+// Applying style and return applyed propertie
+public extension StyleableView {
+    
+    @discardableResult
+    func apply<V>(style: StyleGet<Self, V>) -> V {
+        return style(self)
+    }
+}
+
+// MARK: - StyleableView + Array
+
+public extension Array where Element == UIView {
+    
+    func apply(style: Style<Element>) {
+        forEach { $0.apply(style: style) }
+    }
+    
+    func apply(styles: Style<Element>...) {
+        forEach { $0.apply(styles: styles) }
+    }
+    
+    func apply(styles: [Style<Element>]) {
+        forEach { $0.apply(styles: styles) }
+    }
+}
+
+// MARK: - Predifined styles
+
+public typealias SView = Styles.UIView
+public typealias SButton = Styles.UIButton
+public typealias SLabel = Styles.UILabel
+public typealias STextField = Styles.UITextField
+public typealias STextView = Styles.UITextView
+
+public struct Styles {
+    
+    public struct UIView { }
+    
+    public struct UIButton { }
+    
+    public struct UILabel { }
+    
+    public struct UITextField { }
+    
+    public struct UITextView { }
 }
